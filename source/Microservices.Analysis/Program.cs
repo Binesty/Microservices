a4be677021ebb006e222fb108c8f674c66cb0a3b
Vladimir Matos <vladimir.matos@outlook.com>
2023-08-18 17:22:44 -0300

    docs: initial commit project structure

---
+++ source/Microservices.Analysis/Program.cs
@@ -0,0 +1,17 @@
+namespace Microservices.Analysis
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            IHost host = Host.CreateDefaultBuilder(args)
+                .ConfigureServices(services =>
+                {
+                    services.AddHostedService<Worker>();
+                })
+                .Build();
+
+            host.Run();
+        }
+    }
+}
