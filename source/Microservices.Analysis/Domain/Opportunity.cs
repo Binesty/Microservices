ede18ee944554c344ed20350dc7f2ea39b33e04e
Vladimir Matos <vladimir.matos@outlook.com>
2023-08-27 11:24:46 -0300

    feat: set description opportunity

--- source/Microservices.Analysis/Domain/Opportunity.cs
+++ source/Microservices.Analysis/Domain/Opportunity.cs
@@ -5,5 +5,7 @@ namespace Microservices.Analysis.Domain
     public class Opportunity : Context
     {
         public required string Category { get; set; }
+
+        public required string Description { get; set; }
     }
 }
