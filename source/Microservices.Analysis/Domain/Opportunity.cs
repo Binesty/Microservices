7d3f87972df8d8035148d4442ce058b4c4eecb9b
Vladimir Matos <vladimir.matos@outlook.com>
2023-08-28 13:13:45 -0300

    fix: set a manual fix to verify version increment #25

--- source/Microservices.Analysis/Domain/Opportunity.cs
+++ source/Microservices.Analysis/Domain/Opportunity.cs
@@ -4,8 +4,11 @@ namespace Microservices.Analysis.Domain
 {
     public class Opportunity : Context
     {
+        public required string Title { get; set; }
+
         public required string Category { get; set; }
 
         public required string Description { get; set; }
+
     }
 }
